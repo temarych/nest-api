7121e94ef67a16d823ecceb7e4d33f867797768a
temarych <teemest06@icloud.com>
2024-05-06 14:32:28 +0300

    fix(formatting): add dist to ignore patterns

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,14 +15,14 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'eol-last': ['error', 'always'],
-    'semi': ['error', 'always'],
-    'quotes': ['error', 'single']
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
   },
 };
